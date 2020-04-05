b3ea7a2cd9c10d7c88aa3118fa9a69e7625f07c1
Jean-Eric Cuendet <jean-eric.cuendet@vd.ch>
2020-04-05 15:52:40 +0200

    Premier jet du site

--- web/fabriquer_pain.php
+++ web/fabriquer_pain.php
@@ -28,28 +28,42 @@ include "header.php";
                 <div class="row justify-content-start py-5">
                     <div class="col-md-12 heading-section ftco-animate">
                         <span class="subheading">Les étapes</span>
-                        <h2 class="mb-4">Le pain au levain, en 5 étapes</h2>
-                        <p>Il y a 2 étapes principales</p>
+                        <h2 class="mb-4">Le pain au levain, en 7 étapes</h2>
                         <div class="services-wrap">
-                            <a href="#" class="services-list">Gutter cleaning
-                                <div class="btn-custom d-flex align-items-center justify-content-center"><span
-                                        class="ion-ios-arrow-round-forward"></span></div>
+                            <a href="#etape1" class="services-list">Rafraichir le levain
+                                <div class="btn-custom d-flex align-items-center justify-content-center">
+                                    <span class="ion-ios-arrow-round-forward"></span>
+                                </div>
                             </a>
-                            <a href="#" class="services-list">Organinc fertilization and weed control
-                                <div class="btn-custom d-flex align-items-center justify-content-center"><span
-                                        class="ion-ios-arrow-round-forward"></span></div>
+                            <a href="#etape2" class="services-list">Fabriquer la pâte
+                                <div class="btn-custom d-flex align-items-center justify-content-center">
+                                    <span class="ion-ios-arrow-round-forward"></span>
+                                </div>
                             </a>
-                            <a href="#" class="services-list">Compost top dressing &amp; compost tea application
-                                <div class="btn-custom d-flex align-items-center justify-content-center"><span
-                                        class="ion-ios-arrow-round-forward"></span></div>
+                            <a href="#etape3" class="services-list">Repli du matin
+                                <div class="btn-custom d-flex align-items-center justify-content-center">
+                                    <span class="ion-ios-arrow-round-forward"></span>
+                                </div>
                             </a>
-                            <a href="#" class="services-list">New lawn installation
-                                <div class="btn-custom d-flex align-items-center justify-content-center"><span
-                                        class="ion-ios-arrow-round-forward"></span></div>
+                            <a href="#etape4" class="services-list">Façonnage de milieu de journée
+                                <div class="btn-custom d-flex align-items-center justify-content-center">
+                                    <span class="ion-ios-arrow-round-forward"></span>
+                                </div>
                             </a>
-                            <a href="#" class="services-list">Mulch and other services available
-                                <div class="btn-custom d-flex align-items-center justify-content-center"><span
-                                        class="ion-ios-arrow-round-forward"></span></div>
+                            <a href="#etape5" class="services-list">Cuisson
+                                <div class="btn-custom d-flex align-items-center justify-content-center">
+                                    <span class="ion-ios-arrow-round-forward"></span>
+                                </div>
+                            </a>
+                            <a href="#etape6" class="services-list">Fin de la cuisson
+                                <div class="btn-custom d-flex align-items-center justify-content-center">
+                                    <span class="ion-ios-arrow-round-forward"></span>
+                                </div>
+                            </a>
+                            <a href="#etape7" class="services-list">Découpage du pain
+                                <div class="btn-custom d-flex align-items-center justify-content-center">
+                                    <span class="ion-ios-arrow-round-forward"></span>
+                                </div>
                             </a>
                         </div>
                     </div>
@@ -59,6 +73,181 @@ include "header.php";
     </div>
 </section>
 
+
+<section class="ftco-section" id="etape1">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-10 text-center heading-section ftco-animate">
+                <span class="subheading">Etape 1</span>
+                <h2 class="mb-4">Rafraichir le levain</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="item">
+                    <div class="wrap">
+                        <div class="seasonal img d-flex align-items-center justify-content-center">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/iBkjf15utWk"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section" id="etape2">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-10 text-center heading-section ftco-animate">
+                <span class="subheading">Etape 2</span>
+                <h2 class="mb-4">Fabriquer la pâte</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="item">
+                    <div class="wrap">
+                        <div class="seasonal img d-flex align-items-center justify-content-center">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/ZhxAxsiggb8"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section" id="etape3">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-10 text-center heading-section ftco-animate">
+                <span class="subheading">Etape 3</span>
+                <h2 class="mb-4">Repli du matin</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="item">
+                    <div class="wrap">
+                        <div class="seasonal img d-flex align-items-center justify-content-center">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/ZhxAxsiggb8"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section" id="etape4">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-10 text-center heading-section ftco-animate">
+                <span class="subheading">Etape 4</span>
+                <h2 class="mb-4">Façonnage du milieu de journée</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="item">
+                    <div class="wrap">
+                        <div class="seasonal img d-flex align-items-center justify-content-center">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/ZhxAxsiggb8"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section" id="etape5">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-10 text-center heading-section ftco-animate">
+                <span class="subheading">Etape 5</span>
+                <h2 class="mb-4">Cuisson</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="item">
+                    <div class="wrap">
+                        <div class="seasonal img d-flex align-items-center justify-content-center">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/ZhxAxsiggb8"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section" id="etape6">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-10 text-center heading-section ftco-animate">
+                <span class="subheading">Etape 6</span>
+                <h2 class="mb-4">Fin de la cuisson</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="item">
+                    <div class="wrap">
+                        <div class="seasonal img d-flex align-items-center justify-content-center">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/M0_9x0JZInw"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section" id="etape7">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-10 text-center heading-section ftco-animate">
+                <span class="subheading">Etape 7</span>
+                <h2 class="mb-4">Découpage du pain</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="item">
+                    <div class="wrap">
+                        <div class="seasonal img d-flex align-items-center justify-content-center">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/TB6p3ntD0Ng"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
 include "footer.php";
